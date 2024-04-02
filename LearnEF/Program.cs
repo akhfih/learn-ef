42ee7e3bad644c9cce5f442c19d1529ea774c243
Akhmad Fikron Huda <akhmad.fikron.huda@gmail.com>
2024-04-02 16:18:13 +0700

    get byid and get all

--- LearnEF/Program.cs
+++ LearnEF/Program.cs
@@ -7,16 +7,34 @@ public class Program
     public static void Main(string[] args)
     {
         using AppDbContext context = new();
-        Customer customer = new()
+
+        // get by id
+        var customer = context.Customers.FirstOrDefault(customer => customer.Id.Equals(1));
+        Console.WriteLine($"Customer: Id{customer.Id}, Name: {customer.CustomerName}, " + "" +
+                          $"Mobile Phone: {customer.MobilePhone}, Email: {customer.MobilePhone}");
+        
+        Console.WriteLine(context.Entry(customer).State);
+
+        // get all
+        var customers = context.Customers.ToList();
+        
+        foreach (var c in customers)
         {
-            CustomerName = "Akhmad",
-            Address = "Jl. H. Dahlan",
-            MobilePhone = "085742553119",
-            Email = "akhmad@gmail.com"
-            
+            Console.WriteLine($"Customer: Id{c.Id}, Name: {c.CustomerName}, " + "" +
+                              $"Mobile Phone: {c.MobilePhone}, Email: {c.MobilePhone}");
+        }
+
+        // create
+        /*Customer newCustomer = new()
+        {
+            CustomerName = "Fikron",
+            Address = "Jl. Melati",
+            MobilePhone = "0857425531193",
+            Email = "fikron@gmail.com"
+
         };
 
-        context.Customers.Add(customer);
-        context.SaveChanges();
+        context.Customers.Add(newCustomer);
+        context.SaveChanges();*/
     }
 }
