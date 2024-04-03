a8ed13206f471c5db9edcf9e4f676821da89a072
Akhmad Fikron Huda <akhmad.fikron.huda@gmail.com>
2024-04-03 17:25:42 +0700

    entity Product, Purchase, purchaseDetail relasi

---
+++ LearnEF/Entities/Purchase.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnEF.Entities;
+
+public class Purchase
+{
+    [Key, Column(name: "id")]
+    public Guid Id { get; set; }
+    [Column(name: "trans_date")]
+    public DateTime TransDate { get; set; }
+    [Column(name: "customer_id")]
+    public Guid CustomerId { get; set; }
+    
+    public virtual Customer Customer { get; set; }
+    public virtual ICollection<PurchaseDetail> PurchaseDetails { get; set; }
+}
---
+++ LearnEF/Entities/PurchaseDetail.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnEF.Entities;
+
+[Table(name:"t_purchase_detail")]
+public class PurchaseDetail
+{
+    [Key, Column(name: "id")]
+    public Guid Id { get; set; }
+    
+    [Column(name:"purchase_id")]
+    public Guid PurchaseId { get; set; }
+    [Column(name:"product_id")]
+    public Guid ProductId { get; set; }
+    [Column(name:"qty")]
+    public int Qty { get; set; }
+
+    public Purchase Purchase { get; set; }
+    public Purchase Product { get; set; }
+}
